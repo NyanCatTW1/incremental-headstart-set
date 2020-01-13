4b52941c754a4ec2bdfae1c8d97d80735844b7f9
Nyan cat <a0939712328@gmail.com>
2020-01-13 16:49:37 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function getDefaultPlayer = () => ({
+  lastUpdate: new Date().getTime()
+})
+player = getDefaultPlayer()
+var diffMultiplier = 1
+let gameLoopIntervalId = 0
+
+function updateDisplay() {
+  // read title
+}
+
+function gameLoop() {
+  // 1 diff = 0.001 seconds
+  var thisUpdate = new Date().getTime()
+  if (typeof diff === "undefined") var diff = Math.min(thisUpdate - player.lastUpdate, 21600000)
+  diff *= diffMultiplier
+  //if (diffMultiplier > 1) console.log("SHAME")
+  //else if (diffMultiplier < 1) console.log("SLOWMOTION")
+
+  updateDisplay()
+}
+
+function startGame() {
+  // Some init job
+  startInterval()
+}
+
+function startInterval() {
+  gameLoopIntervalId = setInterval(gameLoop, 33)
+}
